c6006eb12730012c6d7920c01ed2996fa2126d3b
vanajachowdary16 <vnajachowdary12@gmail.com>
2025-05-17 19:59:36 +0530

    learnt about strings manipulation

---
+++ Strings.java
@@ -0,0 +1,31 @@
+import java.lang.Object;
+class Main {
+    public static void main(String[] args) {
+    String s = "example";
+    String reverse="";
+    for(int i=s.length()-1;i>=0; i--){
+        reverse= reverse+ s.charAt(i);
+    }
+    System.out.println(reverse);
+    System.out.println(reverse.toUpperCase());
+    System.out.println(reverse.toLowerCase());
+    System.out.println(s.indexOf("a"));
+    System.out.println(s.isEmpty());
+    System.out.println(s.isBlank());
+    char[] newChar = s.toCharArray();
+    System.out.println(newChar[1]);
+    String secondstring = "exaample";
+    System.out.println(s.equalsIgnoreCase(secondstring));
+    System.out.println(s+" "+secondstring);
+    System.out.println(s.concat(secondstring));
+    String x="10";
+    String y = s+x;
+    System.out.println(y);
+    System.out.println("learning double quotes\"represents double quotes\"");
+    System.out.println("representing \'singlequote\'");
+    System.out.println("representing \\ backslash ");
+    System.out.println("next\nline ");
+    System.out.println("test\ttab");
+    System.out.println(s.lastIndexOf("a"));
+}
+}
