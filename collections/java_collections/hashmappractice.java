7ebbbb069a136c78d55d71474cf7a2670770f8ba
vanajachowdary16 <vanajachowdary12@gmail.com>
2025-09-01 13:05:04 +0530

    learned hashmap, linked hashmap syntax

---
+++ collections/java_collections/hashmappractice.java
@@ -0,0 +1,24 @@
+package collections.java_collections;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class hashmappractice {
+    public static void main(String[] args){
+        Map<String, Integer> maps = new HashMap<>();
+        maps.put("students", 20);
+        maps.put("name", 01);
+        maps.put("ary", 02);
+        maps.put("rayn", 03);
+        System.out.println("hash map is " +maps);
+
+        Map<String, Integer> citys = new LinkedHashMap<>();
+        citys.put("yes",  01);
+        citys.put("vij", 02);
+        citys.put("hyd", 03);
+        citys.put("nyl", 04);
+        System.out.println("linked hashmap is " +citys);
+    }
+    
+}
