5303b762c6a5e7654ada8c65bc5f084956ea48b4
vanajachowdary16 <vanajachowdary12@gmail.com>
2025-11-16 11:54:22 +0530

    streak

---
+++ interviewprep/anagram.java
@@ -0,0 +1,34 @@
+package interviewprep;
+
+public class anagram {
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        if (areAnagrams(str1, str2)) {
+            System.out.println(str1 + " and " + str2 + " are anagrams.");
+        } else {
+            System.out.println(str1 + " and " + str2 + " are not anagrams.");
+        }
+    }
+    public static boolean areAnagrams(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        int[] charCount = new int[256]; // Assuming ASCII character set
+
+        for (int i = 0; i < str1.length(); i++) {
+            charCount[str1.charAt(i)]++;
+            charCount[str2.charAt(i)]--;
+        }
+
+        for (int count : charCount) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
